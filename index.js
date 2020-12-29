ca5a698718a68d58ef7479bfe3c23f5044871b6c
PrinceSP <princepasombaran@gmail.com>
2020-12-29 19:28:13 +0800

    wrap with IIFE and set timeout every 1 second

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const newDate = new Date('1 jan 2021')
-
-
-function counter(){
+(async function counter(){
+  const newDate = new Date('1 jan 2021')
   let currentDate = new Date()
   let seconds = Math.round((newDate-currentDate)/1000)%60
   const ps = document.querySelector('.dates')
   ps.children[3].textContent = seconds
-}
-counter()
-// setInterval(counter,1000)
+  setTimeout(counter,1000)
+}())
